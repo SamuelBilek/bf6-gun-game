11c1be91c1ed3aa2ebcbc0b8099da2810e7a5f76
Samuel Bilek <sam@polygoniq.com>
2026-07-11 21:01:31 +0200

    Update to work with latest SDK version
    Commented out glitching UI code for now

--- GunGame.ts
+++ GunGame.ts
@@ -1,3 +1,5 @@
+import * as modlib from "modlib";
+
 const WEAPON_LEVELS = 15;
 const AVAILABLE_MELEE = [
     mod.Gadgets.Melee_Combat_Knife, 
@@ -5,7 +7,9 @@ const AVAILABLE_MELEE = [
     mod.Gadgets.Melee_Sledgehammer
 ];
 const MELEE = AVAILABLE_MELEE[Math.floor(Math.random() * AVAILABLE_MELEE.length)];
-const WEAPON_VALUES = (Object.keys(mod.Weapons) as Array<keyof typeof mod.Weapons>).filter(k => isNaN(Number(k))).map(k => mod.Weapons[k]) as mod.Weapons[];
+const WEAPON_VALUES = (Object.keys(mod.Weapons) as Array<keyof typeof mod.Weapons>)
+    .filter(k => isNaN(Number(k)) && !String(k).startsWith("BattlePickup"))
+    .map(k => mod.Weapons[k]) as mod.Weapons[];
 const INVENTORY_SLOTS = (Object.keys(mod.InventorySlots) as Array<keyof typeof mod.InventorySlots>).filter(k => isNaN(Number(k))).map(k => mod.InventorySlots[k]) as mod.InventorySlots[];
 
 var AVAILABLE_WEAPONS: mod.Weapons[] = [];
@@ -69,7 +73,7 @@ class WeaponPlayerCountWidget {
     }
 
     CreateStaticWidget() {
-        this.staticWidget = ParseUI({
+        this.staticWidget = modlib.ParseUI({
             type: "Container",
             position: [0, PROGRESS_BAR_Y_OFFSET + WEAPON_ICON_ITEM_HEIGHT],
             size: [PROGRESS_BAR_WIDTH, WEAPON_COUNT_ITEM_HEIGHT],
@@ -77,7 +81,7 @@ class WeaponPlayerCountWidget {
             bgFill: mod.UIBgFill.None,
         });
         for (let i = 0; i < AVAILABLE_WEAPONS.length; i++) {
-            ParseUI({
+            modlib.ParseUI({
                 type: "Container",
                 position: [i * PROGRESS_BAR_ITEM_WIDTH, 0],
                 size: [PROGRESS_BAR_ITEM_WIDTH, WEAPON_COUNT_ITEM_HEIGHT],
@@ -86,7 +90,7 @@ class WeaponPlayerCountWidget {
                 bgColor: PROGRESS_BAR_ITEM_BASE_COLOR,
                 bgFill: mod.UIBgFill.Blur,
             });
-            ParseUI({
+            modlib.ParseUI({
                 type: "Container",
                 position: [(i + 1) * PROGRESS_BAR_ITEM_WIDTH, 0],
                 size: [SEPARATOR_WIDTH, WEAPON_COUNT_ITEM_HEIGHT],
@@ -96,7 +100,7 @@ class WeaponPlayerCountWidget {
                 parent: this.staticWidget,
             });
         }
-        ParseUI({
+        modlib.ParseUI({
             type: "Container",
             position: [AVAILABLE_WEAPONS.length * PROGRESS_BAR_ITEM_WIDTH, 0],
             size: [PROGRESS_BAR_ITEM_WIDTH, WEAPON_COUNT_ITEM_HEIGHT],
@@ -117,13 +121,13 @@ class WeaponPlayerCountWidget {
                 mod.DeleteUIWidget(widget);
             }
             if (refreshNeeded) {
-                widget = ParseUI({
+                widget = modlib.ParseUI({
                     type: "Text",
                     position: [i * PROGRESS_BAR_ITEM_WIDTH, 0],
                     size: [PROGRESS_BAR_ITEM_WIDTH, WEAPON_COUNT_ITEM_HEIGHT],
                     anchor: mod.UIAnchor.TopLeft,
                     textAnchor: mod.UIAnchor.Center,
-                    textLabel: MakeMessage("${}", WEAPON_TO_PLAYER_COUNT_MAP.get(weapon) ?? 0),
+                    textLabel: String(currentCount),
                     parent: this.staticWidget,
                 });
                 this.playerCountWidgets.set(weapon, widget!);
@@ -135,13 +139,13 @@ class WeaponPlayerCountWidget {
             mod.DeleteUIWidget(this.meleeCountWidget);
         }
         if (refreshNeeded) {
-            this.meleeCountWidget = ParseUI({
+            this.meleeCountWidget = modlib.ParseUI({
                 type: "Text",
                 position: [AVAILABLE_WEAPONS.length * PROGRESS_BAR_ITEM_WIDTH, 0],
                 size: [PROGRESS_BAR_ITEM_WIDTH, WEAPON_COUNT_ITEM_HEIGHT],
                 anchor: mod.UIAnchor.TopLeft,
                 textAnchor: mod.UIAnchor.Center,
-                textLabel: MakeMessage("${}", MELEE_PLAYER_COUNT),
+                textLabel: String(MELEE_PLAYER_COUNT),
                 parent: this.staticWidget,
             });
             this.prevMeleeCount = MELEE_PLAYER_COUNT;
@@ -167,7 +171,7 @@ class WeaponProgressWidget {
     }
 
     CreateStaticWidget() {
-        this.staticWidget = ParseUI({
+        this.staticWidget = modlib.ParseUI({
             type: "Container",
             position: [0, PROGRESS_BAR_Y_OFFSET],
             size: [PROGRESS_BAR_WIDTH, WEAPON_ICON_ITEM_HEIGHT],
@@ -177,7 +181,7 @@ class WeaponProgressWidget {
             bgFill: mod.UIBgFill.None,
         });
         for (const [i, weapon] of AVAILABLE_WEAPONS.entries()) {
-            ParseUI({
+            modlib.ParseUI({
                 type: "Container",
                 position: [(i + 1) * PROGRESS_BAR_ITEM_WIDTH, 0],
                 size: [SEPARATOR_WIDTH, WEAPON_ICON_ITEM_HEIGHT],
@@ -200,7 +204,7 @@ class WeaponProgressWidget {
                 widget = undefined;
             }
             if (!widget) {
-                widget = ParseUI({
+                widget = modlib.ParseUI({
                     type: "Container",
                     position: [i * PROGRESS_BAR_ITEM_WIDTH, 0],
                     size: [PROGRESS_BAR_ITEM_WIDTH, WEAPON_ICON_ITEM_HEIGHT],
@@ -220,7 +224,7 @@ class WeaponProgressWidget {
             this.meleeItemWidget = undefined;
         }
         if (!this.meleeItemWidget) {
-            this.meleeItemWidget = ParseUI({
+            this.meleeItemWidget = modlib.ParseUI({
                 type: "Container",
                 position: [AVAILABLE_WEAPONS.length * PROGRESS_BAR_ITEM_WIDTH, 0],
                 size: [PROGRESS_BAR_ITEM_WIDTH, WEAPON_ICON_ITEM_HEIGHT],
@@ -298,7 +302,10 @@ export async function OnGameModeStarted() {
     mod.SetSpawnMode(mod.SpawnModes.AutoSpawn);
     CreateAvailableWeapons();
     GLOBAL_UI_REFRESH_NEEDED = true;
-    WEAPON_PLAYER_COUNT_WIDGET = new WeaponPlayerCountWidget();
+    // For some reason with the later version of the SDK, displaying using the modlib.ParseUI() fails to display the player count
+    // Instead it shows <unknown_string>
+    // TODO: Uncomment this when the display bug is fixed
+    //WEAPON_PLAYER_COUNT_WIDGET = new WeaponPlayerCountWidget();
 }
 
 
@@ -385,338 +392,4 @@ export function OnPlayerEarnedKill(
     GLOBAL_UI_REFRESH_NEEDED = true;
 }
 
-
-//-----------------------------------------------------------------------------------------------//
-//-----------------------------------------------------------------------------------------------//
-//-----------------------------------------------------------------------------------------------//
-//-----------------------------------------------------------------------------------------------//
-// Helper functions to create UI from a JSON object tree:
-//-----------------------------------------------------------------------------------------------//
-
-type UIVector = mod.Vector | number[];
-
-interface UIParams {
-    name: string;
-    type: string;
-    position: any;
-    size: any;
-    anchor: mod.UIAnchor;
-    parent: mod.UIWidget;
-    visible: boolean;
-    textLabel: string;
-    textColor: UIVector;
-    textAlpha: number;
-    textSize: number;
-    textAnchor: mod.UIAnchor;
-    padding: number;
-    bgColor: UIVector;
-    bgAlpha: number;
-    bgFill: mod.UIBgFill;
-    imageType: mod.UIImageType;
-    imageColor: UIVector;
-    imageAlpha: number;
-    teamId?: mod.Team;
-    playerId?: mod.Player;
-    children?: any[];
-    buttonEnabled: boolean;
-    buttonColorBase: UIVector;
-    buttonAlphaBase: number;
-    buttonColorDisabled: UIVector;
-    buttonAlphaDisabled: number;
-    buttonColorPressed: UIVector;
-    buttonAlphaPressed: number;
-    buttonColorHover: UIVector;
-    buttonAlphaHover: number;
-    buttonColorFocused: UIVector;
-    buttonAlphaFocused: number;
-}
-
-function __asModVector(param: number[]|mod.Vector) {
-    if (Array.isArray(param))
-        return mod.CreateVector(param[0], param[1], param.length == 2 ? 0 : param[2]);
-    else
-        return param;
-}
-
-function __asModMessage(param: string|mod.Message) {
-    if (typeof (param) === "string")
-        return mod.Message(param);
-    return param;
-}
-
-function __fillInDefaultArgs(params: UIParams) {
-    if (!params.hasOwnProperty('name'))
-        params.name = "";
-    if (!params.hasOwnProperty('position'))
-        params.position = mod.CreateVector(0, 0, 0);
-    if (!params.hasOwnProperty('size'))
-        params.size = mod.CreateVector(100, 100, 0);
-    if (!params.hasOwnProperty('anchor'))
-        params.anchor = mod.UIAnchor.TopLeft;
-    if (!params.hasOwnProperty('parent'))
-        params.parent = mod.GetUIRoot();
-    if (!params.hasOwnProperty('visible'))
-        params.visible = true;
-    if (!params.hasOwnProperty('padding'))
-        params.padding = (params.type == "Container") ? 0 : 8;
-    if (!params.hasOwnProperty('bgColor'))
-        params.bgColor = mod.CreateVector(0.25, 0.25, 0.25);
-    if (!params.hasOwnProperty('bgAlpha'))
-        params.bgAlpha = 0.5;
-    if (!params.hasOwnProperty('bgFill'))
-        params.bgFill = mod.UIBgFill.Solid;
-}
-
-function __setNameAndGetWidget(uniqueName: any, params: any) {
-    let widget = mod.FindUIWidgetWithName(uniqueName) as mod.UIWidget;
-    mod.SetUIWidgetName(widget, params.name);
-    return widget;
-}
-
-const __cUniqueName = "----uniquename----";
-
-function __addUIContainer(params: UIParams) {
-    __fillInDefaultArgs(params);
-    let restrict = params.teamId ?? params.playerId;
-    if (restrict) {
-        mod.AddUIContainer(__cUniqueName,
-            __asModVector(params.position),
-            __asModVector(params.size),
-            params.anchor,
-            params.parent,
-            params.visible,
-            params.padding,
-            __asModVector(params.bgColor),
-            params.bgAlpha,
-            params.bgFill,
-            restrict);
-    } else {
-        mod.AddUIContainer(__cUniqueName,
-            __asModVector(params.position),
-            __asModVector(params.size),
-            params.anchor,
-            params.parent,
-            params.visible,
-            params.padding,
-            __asModVector(params.bgColor),
-            params.bgAlpha,
-            params.bgFill);
-    }
-    let widget = __setNameAndGetWidget(__cUniqueName, params);
-    if (params.children) {
-        params.children.forEach((childParams: any) => {
-            childParams.parent = widget;
-            __addUIWidget(childParams);
-        });
-    }
-    return widget;
-}
-
-function __fillInDefaultTextArgs(params: UIParams) {
-    if (!params.hasOwnProperty('textLabel'))
-        params.textLabel = "";
-    if (!params.hasOwnProperty('textSize'))
-        params.textSize = 0;
-    if (!params.hasOwnProperty('textColor'))
-        params.textColor = mod.CreateVector(1, 1, 1);
-    if (!params.hasOwnProperty('textAlpha'))
-        params.textAlpha = 1;
-    if (!params.hasOwnProperty('textAnchor'))
-        params.textAnchor = mod.UIAnchor.CenterLeft;
-}
-
-function __addUIText(params: UIParams) {
-    __fillInDefaultArgs(params);
-    __fillInDefaultTextArgs(params);
-    let restrict = params.teamId ?? params.playerId;
-    if (restrict) {
-        mod.AddUIText(__cUniqueName,
-            __asModVector(params.position),
-            __asModVector(params.size),
-            params.anchor,
-            params.parent,
-            params.visible,
-            params.padding,
-            __asModVector(params.bgColor),
-            params.bgAlpha,
-            params.bgFill,
-            __asModMessage(params.textLabel),
-            params.textSize,
-            __asModVector(params.textColor),
-            params.textAlpha,
-            params.textAnchor,
-            restrict);
-    } else {
-        mod.AddUIText(__cUniqueName,
-            __asModVector(params.position),
-            __asModVector(params.size),
-            params.anchor,
-            params.parent,
-            params.visible,
-            params.padding,
-            __asModVector(params.bgColor),
-            params.bgAlpha,
-            params.bgFill,
-            __asModMessage(params.textLabel),
-            params.textSize,
-            __asModVector(params.textColor),
-            params.textAlpha,
-            params.textAnchor);
-    }
-    return __setNameAndGetWidget(__cUniqueName, params);
-}
-
-function __fillInDefaultImageArgs(params: any) {
-    if (!params.hasOwnProperty('imageType'))
-        params.imageType = mod.UIImageType.None;
-    if (!params.hasOwnProperty('imageColor'))
-        params.imageColor = mod.CreateVector(1, 1, 1);
-    if (!params.hasOwnProperty('imageAlpha'))
-        params.imageAlpha = 1;
-}
-
-function __addUIImage(params: UIParams) {
-    __fillInDefaultArgs(params);
-    __fillInDefaultImageArgs(params);
-    let restrict = params.teamId ?? params.playerId;
-    if (restrict) {
-        mod.AddUIImage(__cUniqueName,
-            __asModVector(params.position),
-            __asModVector(params.size),
-            params.anchor,
-            params.parent,
-            params.visible,
-            params.padding,
-            __asModVector(params.bgColor),
-            params.bgAlpha,
-            params.bgFill,
-            params.imageType,
-            __asModVector(params.imageColor),
-            params.imageAlpha,
-            restrict);
-    } else {
-        mod.AddUIImage(__cUniqueName,
-            __asModVector(params.position),
-            __asModVector(params.size),
-            params.anchor,
-            params.parent,
-            params.visible,
-            params.padding,
-            __asModVector(params.bgColor),
-            params.bgAlpha,
-            params.bgFill,
-            params.imageType,
-            __asModVector(params.imageColor),
-            params.imageAlpha);
-    }
-    return __setNameAndGetWidget(__cUniqueName, params);
-}
-
-function __fillInDefaultArg(params: any, argName: any, defaultValue: any) {
-    if (!params.hasOwnProperty(argName))
-        params[argName] = defaultValue;
-}
-
-function __fillInDefaultButtonArgs(params: any) {
-    if (!params.hasOwnProperty('buttonEnabled'))
-        params.buttonEnabled = true;
-    if (!params.hasOwnProperty('buttonColorBase'))
-        params.buttonColorBase = mod.CreateVector(0.7, 0.7, 0.7);
-    if (!params.hasOwnProperty('buttonAlphaBase'))
-        params.buttonAlphaBase = 1;
-    if (!params.hasOwnProperty('buttonColorDisabled'))
-        params.buttonColorDisabled = mod.CreateVector(0.2, 0.2, 0.2);
-    if (!params.hasOwnProperty('buttonAlphaDisabled'))
-        params.buttonAlphaDisabled = 0.5;
-    if (!params.hasOwnProperty('buttonColorPressed'))
-        params.buttonColorPressed = mod.CreateVector(0.25, 0.25, 0.25);
-    if (!params.hasOwnProperty('buttonAlphaPressed'))
-        params.buttonAlphaPressed = 1;
-    if (!params.hasOwnProperty('buttonColorHover'))
-        params.buttonColorHover = mod.CreateVector(1,1,1);
-    if (!params.hasOwnProperty('buttonAlphaHover'))
-        params.buttonAlphaHover = 1;
-    if (!params.hasOwnProperty('buttonColorFocused'))
-        params.buttonColorFocused = mod.CreateVector(1,1,1);
-    if (!params.hasOwnProperty('buttonAlphaFocused'))
-        params.buttonAlphaFocused = 1;
-}
-
-function __addUIButton(params: UIParams) {
-    __fillInDefaultArgs(params);
-    __fillInDefaultButtonArgs(params);
-    let restrict = params.teamId ?? params.playerId;
-    if (restrict) {
-        mod.AddUIButton(__cUniqueName,
-            __asModVector(params.position),
-            __asModVector(params.size),
-            params.anchor,
-            params.parent,
-            params.visible,
-            params.padding,
-            __asModVector(params.bgColor),
-            params.bgAlpha,
-            params.bgFill,
-            params.buttonEnabled,
-            __asModVector(params.buttonColorBase), params.buttonAlphaBase,
-            __asModVector(params.buttonColorDisabled), params.buttonAlphaDisabled,
-            __asModVector(params.buttonColorPressed), params.buttonAlphaPressed,
-            __asModVector(params.buttonColorHover), params.buttonAlphaHover,
-            __asModVector(params.buttonColorFocused), params.buttonAlphaFocused,
-            restrict);
-    } else {
-        mod.AddUIButton(__cUniqueName,
-            __asModVector(params.position),
-            __asModVector(params.size),
-            params.anchor,
-            params.parent,
-            params.visible,
-            params.padding,
-            __asModVector(params.bgColor),
-            params.bgAlpha,
-            params.bgFill,
-            params.buttonEnabled,
-            __asModVector(params.buttonColorBase), params.buttonAlphaBase,
-            __asModVector(params.buttonColorDisabled), params.buttonAlphaDisabled,
-            __asModVector(params.buttonColorPressed), params.buttonAlphaPressed,
-            __asModVector(params.buttonColorHover), params.buttonAlphaHover,
-            __asModVector(params.buttonColorFocused), params.buttonAlphaFocused);
-    }
-    return __setNameAndGetWidget(__cUniqueName, params);
-}
-
-function __addUIWidget(params: UIParams) {
-    if (params == null)
-        return undefined;
-    if (params.type == "Container")
-        return __addUIContainer(params);
-    else if (params.type == "Text")
-        return __addUIText(params);
-    else if (params.type == "Image")
-        return __addUIImage(params);
-    else if (params.type == "Button")
-        return __addUIButton(params);
-    return undefined;
-}
-
-export function ParseUI(...params: any[]) {
-    let widget: mod.UIWidget|undefined;
-    for (let a = 0; a < params.length; a++) {
-        widget = __addUIWidget(params[a] as UIParams);
-    }
-    return widget;
-}
-
-function MakeMessage(message: string, ...args: any[]) {
-    switch (args.length) {
-        case 0:
-            return mod.Message(message);
-        case 1:
-            return mod.Message(message, args[0]);
-        case 2:
-            return mod.Message(message, args[0], args[1]);
-        default:
-            return mod.Message(message, args[0], args[1], args[2]);
-    }
-}
 
